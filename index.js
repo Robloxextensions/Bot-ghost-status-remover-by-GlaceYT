5179d3435c25cf0a947f1b7ecf46e8a304b450ed
Robloxextensions <132360328+Robloxextensions@users.noreply.github.com>
2025-04-08 17:22:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Watching over Sun's Lounge"];
+const statusMessages = ["Watching over Overwatch Office"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
